bf951da149d71eb1de24bd3b223c58d12acbacf4
s1360844 <146123883+s1360844@users.noreply.github.com>
2023-10-04 21:57:46 -0400

    Add files via upload
    All outputs are displayed when entering an input value even though I set "if" statements and such to prohibit this from happening. Your assistance would be greatly appreciated.

---
+++ conditions.py
@@ -0,0 +1,36 @@
+# CS104
+# Jared Mishen
+# conditions.py
+
+temp = 0
+#print(temp)
+a = int(input("Please enter the current temperature: "))
+if (a >= 90):
+    print("Wear shorts")
+elif (a < 90):
+    print("Short sleeves are fine")
+
+
+
+if (a >= 70) :
+    print("Short sleeves are fine")
+elif (a < 70) :
+    print("Wear a jacket")
+
+
+
+if (a >= 50):
+    print("Wear a jacket")
+elif (a < 50):
+    print("Wear a heavy coat")
+
+
+
+if (a >= 32):
+    print("Wear a heavy coat")
+elif (a < 32):
+    print("Stay inside")
+
+
+        
+
